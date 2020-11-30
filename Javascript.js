35441c937768f49e13bbbc7d9a056698088669d0
FABIEN-VEND <74510132+FABIEN-VEND@users.noreply.github.com>
2020-11-30 17:37:54 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,16 @@
+
+
+
+//ceci est aussi commentaire sur une ligne//
+/*
+ceci est un 
+commentaire sur 
+plusieurs ligne
+*/
+window.alert("test");
+// declartion//
+let affichertexte;
+ //affectation//
+ affichertexte = "mon texte à afficher";
+
+ alert("affichertexte");
